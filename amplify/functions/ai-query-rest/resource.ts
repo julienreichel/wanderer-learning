7b579bbadfa9e4e4e03c250c69888fd3e45fcea9
Julien Reichel <julien.reichel@spotme.com>
2024-06-13 21:09:12 +0200

    fix: increasing timeout to 60s

--- amplify/functions/ai-query-rest/resource.ts
+++ amplify/functions/ai-query-rest/resource.ts
@@ -2,7 +2,7 @@ import { defineFunction, secret } from "@aws-amplify/backend";
 
 export const AIQueryRest = defineFunction({
   name: "ai-query-rest",
-  timeoutSeconds: 30, // more than 30s is useless, because AppSync has a hard 30s timeout
+  timeoutSeconds: 60,
   environment: {
     OPENAI_API_KEY: secret("OPENAI_API_KEY"),
     OPENAI_MODEL: process.env.OPENAI_MODEL || "gpt-3.5-turbo",
