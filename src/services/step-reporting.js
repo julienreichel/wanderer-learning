88aacc2b616e2bb647a0bf66201090c1c6247373
Julien Reichel <julien.reichel@spotme.com>
2024-06-14 15:36:56 +0200

    fix: wrong number of points when there was no quiz present in the part

--- src/services/step-reporting.js
+++ src/services/step-reporting.js
@@ -200,7 +200,7 @@ export default class StepReportingService extends ServicePrototype {
       (acc, part) => acc + (part.divisor ? 1 : 0),
       0,
     );
-    const averagePoints = divisor ? averageSumPoints / divisor : 1;
+    const averagePoints = divisor ? averageSumPoints / divisor : 10;
 
     return { totalPoints, averagePoints, divisor };
   }
