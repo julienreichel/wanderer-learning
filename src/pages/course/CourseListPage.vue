4e8c9dabea8601aff28d36af4482cd9a41ecb909
Julien Reichel <julien.reichel@spotme.com>
2024-06-15 14:12:46 +0200

    fix: keep the same layout for all for now

--- src/pages/course/CourseListPage.vue
+++ src/pages/course/CourseListPage.vue
@@ -1,33 +1,30 @@
 <template>
-  <q-page class="q-pa-md q-col-gutter-sm row">
-    <div v-for="course in courses" :key="course.id" class="col-4">
-      <q-card class="full-height" clickable @click="viewCourse(course)">
-        <q-card-section>
-          <div class="text-h5">{{ course.title }}</div>
-        </q-card-section>
-        <q-card-actions>
-          <q-space />
-          <q-btn size="sm" icon="east" @click="viewCourse(course)" />
-        </q-card-actions>
-      </q-card>
-    </div>
-    <div class="col-12">
-      <q-card v-if="userAttributes.isTeacher">
-        <q-card-section>
-          <q-input
-            outlined
-            v-model="newTitle"
-            :label="$t('course.form.add')"
-            @keydown.enter="addCourse()"
-          >
-          </q-input>
-        </q-card-section>
-        <q-card-actions>
-          <q-space />
-          <q-btn size="sm" icon="add" @click="addCourse()" />
-        </q-card-actions>
-      </q-card>
-    </div>
+  <q-page class="q-pa-md q-gutter-sm">
+    <q-card class="full-height" v-for="course in courses" :key="course.id" clickable @click="viewCourse(course)">
+      <q-card-section>
+        <div class="text-h5">{{ course.title }}</div>
+      </q-card-section>
+      <q-card-actions>
+        <q-space />
+        <q-btn size="sm" icon="east" @click="viewCourse(course)" />
+      </q-card-actions>
+    </q-card>
+    <q-card v-if="userAttributes.isTeacher">
+      <q-card-section>
+        <q-input
+          outlined
+          v-model="newTitle"
+          :label="$t('course.form.add')"
+          @keydown.enter="addCourse()"
+        >
+        </q-input>
+      </q-card-section>
+      <q-card-actions>
+        <q-space />
+        <q-btn size="sm" icon="add" @click="addCourse()" />
+      </q-card-actions>
+    </q-card>
+
   </q-page>
 </template>
 
