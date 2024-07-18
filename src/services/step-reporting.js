b3e4c5007ad032f94ecc33d27e63236c1f5165fe
Julien Reichel <julien.reichel@spotme.com>
2024-07-18 09:59:12 +0200

    fix: max is now 5, not 10

--- src/services/step-reporting.js
+++ src/services/step-reporting.js
@@ -200,7 +200,7 @@ export default class StepReportingService extends ServicePrototype {
       (acc, part) => acc + (part.divisor ? 1 : 0),
       0,
     );
-    const averagePoints = divisor ? averageSumPoints / divisor : 10;
+    const averagePoints = divisor ? averageSumPoints / divisor : 5;
 
     return { totalPoints, averagePoints, divisor };
   }
