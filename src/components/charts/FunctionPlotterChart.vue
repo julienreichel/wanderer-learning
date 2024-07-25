050a636f33597dcf648c3bbb2b4a1f4f63d1596e
Julien Reichel <julien.reichel@spotme.com>
2024-07-25 18:23:00 +0200

    fix: improves options for graphs

--- src/components/charts/FunctionPlotterChart.vue
+++ src/components/charts/FunctionPlotterChart.vue
@@ -1,13 +1,17 @@
 <template>
-  <link rel="stylesheet" type="text/css" href="https://jsxgraph.org/jsxgraph.org/distrib/jsxgraph.css" />
+  <link
+    rel="stylesheet"
+    type="text/css"
+    href="https://jsxgraph.org/jsxgraph.org/distrib/jsxgraph.css"
+  />
   <div :id="boardId" ref="jxgBoard" class="jxgbox"></div>
 </template>
 
 <script setup>
-import { ref, watch, onBeforeUnmount, nextTick } from 'vue';
-import { uid } from 'quasar';
-import { compile } from 'mathjs';
-import JXG from 'jsxgraph';
+import { ref, watch, onBeforeUnmount, nextTick } from "vue";
+import { uid } from "quasar";
+import { compile } from "mathjs";
+import JXG from "jsxgraph";
 // for some weird reason I cannot get this to work, so hacking using the above link
 //import 'jsxgraph/distrib/jsxgraph.css';
 
@@ -15,63 +19,107 @@ import JXG from 'jsxgraph';
 const props = defineProps({
   functionString: {
     type: String,
-    required: true,
+    default: "",
   },
   controls: {
     type: Object,
     default: () => ({
-      p: { type: 'point', settings: { style: 4 } },
-      a: { type: 'slider', position: [[1,-4.5],[3.9,-4.5],[-5,2,5]] },
-  }),
+      p: { type: "point", settings: { style: 4 } },
+      a: {
+        type: "slider",
+        position: [
+          [1, -4.5],
+          [3.9, -4.5],
+          [-2, 1, 2],
+        ],
+      },
+      g: {
+        type: "glider",
+        position: [1, 2],
+        options: { tangent: { dash: 2, strokeColor: "#aa0000" } },
+      },
+    }),
   },
 });
 
 // Reactive reference to the container element
 const jxgBoard = ref(null);
-const boardId = ref('jsxgraph-' + uid());
+const boardId = ref("jsxgraph-" + uid());
 let board;
 
 // Method to render the JSXGraph board
 const renderBoard = (funcStr) => {
-  console.log('Rendering board with function:', funcStr);
+  console.log("Rendering board with function:", funcStr);
   if (board) {
     JXG.JSXGraph.freeBoard(board);
   }
 
-  board = JXG.JSXGraph.initBoard(boardId.value, {boundingbox:[-5,5,5,-5], axis:true});
+  board = JXG.JSXGraph.initBoard(boardId.value, {
+    boundingbox: [-5, 5, 5, -5],
+    axis: true,
+  });
 
   let vars = {};
   Object.keys(props.controls).forEach((key) => {
-    const {type, settings, position} = props.controls[key];
-    vars[key] = board.create(type, position || [0,0], { name: key, ...settings });
-  })
+    const { type, settings, position } = props.controls[key];
+    if (type === "glider") {
+      vars[key] = { elType: "glider", X: () => 0, Y: () => 0 };
+      return;
+    }
+    vars[key] = board.create(type, position || [0, 0], {
+      name: key,
+      ...settings,
+    });
+  });
   if (!funcStr.length) {
     return;
   }
   try {
     // Compile the function string safely using mathjs
     const parsedFunction = compile(funcStr);
-    board.create('functiongraph', [(x) => {
-      const props = { x };
-      Object.keys(vars).forEach((key) => {
-        if (vars[key].elType === 'point') {
-          props[key] = { x: vars[key].X(), y: vars[key].Y() };
-        } else if (vars[key].elType === 'slider') {
-          props[key] = vars[key].Value();
+    const f = board.create("functiongraph", [
+      (x) => {
+        const props = { x };
+        Object.keys(vars).forEach((key) => {
+          if (vars[key].elType === "point" || vars[key].elType === "glider") {
+            props[key] = { x: vars[key].X(), y: vars[key].Y() };
+          } else if (vars[key].elType === "slider") {
+            props[key] = vars[key].Value();
+          }
+        });
+        return parsedFunction.evaluate(props);
+      },
+    ]);
+    Object.keys(props.controls).forEach((key) => {
+      const {
+        type,
+        settings,
+        position = [],
+        options = {},
+      } = props.controls[key];
+      if (type === "glider") {
+        vars[key] = board.create(type, [...position, f], {
+          name: key,
+          ...settings,
+        });
+        if (options.tangent) {
+          board.create("tangent", [vars[key]], options.tangent);
         }
-      });
-      return parsedFunction.evaluate(props);
-    }]);
+      }
+    });
   } catch (e) {
-    console.warn('Invalid function string', e.message);
+    console.warn("Invalid function string", e.message);
   }
 };
 
 // Watch for changes in the functionString prop and re-render the board
-watch(() => props.functionString, (newVal) => {
-  nextTick(() => renderBoard(newVal));
-}, { immediate: true });
-
+watch(
+  () => props.functionString,
+  (newVal) => {
+    nextTick(() => renderBoard(newVal));
+  },
+  { immediate: true },
+);
 
 onBeforeUnmount(() => {
   if (board) {
